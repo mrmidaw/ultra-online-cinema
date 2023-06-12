7273cb1acf1e090253170f32cbd6f5e54fc62c7f
gavrilov <gavrilov@web.1tv.ru>
2023-06-12 15:32:06 +0300

    layout

---
+++ frontend/app/components/screens/home/Home.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+import Layout from "@/components/layout/Layout";
+
+const Home: FC = () => {
+	return (
+		<Layout>
+			<div>Home page</div>
+		</Layout>
+	);
+};
+
+export default Home;
