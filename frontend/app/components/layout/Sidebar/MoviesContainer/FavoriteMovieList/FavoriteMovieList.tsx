7a292809d522edcd3fe9373243ac1a534ade2b86
mrmidaw <mr.midaw@gmail.com>
2023-07-09 12:41:41 +0300

    favorites & 404

---
+++ frontend/app/components/layout/Sidebar/MoviesContainer/FavoriteMovieList/FavoriteMovieList.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+
+import MovieList from "../MovieList";
+
+import NotAuthFavorites from "./NotAuthFavorites";
+import { useFavorites } from "@/components/screens/favorites/useFavorites";
+import SkeletonLoader from "@/components/ui/skeleton-loader/SkeletonLoader";
+import { useAuth } from "@/hooks/useAuth";
+
+const FavoriteMovieList: FC = () => {
+	const { isLoading, favoritesMovies } = useFavorites();
+	const { user } = useAuth();
+
+	if (!user) return <NotAuthFavorites />;
+
+	return isLoading ? (
+		<div className="mt-11">
+			<SkeletonLoader count={3} className="h-28 mb-4" />
+		</div>
+	) : (
+		<MovieList
+			list={{
+				link: "/favorites",
+				movies: favoritesMovies?.slice(0, 3) || [],
+				title: "Favorites",
+			}}
+		/>
+	);
+};
+
+export default FavoriteMovieList;
