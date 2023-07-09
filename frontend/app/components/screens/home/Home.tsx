0a535133c6fa425ddeb7808281f1c375193cc1b1
mrmidaw <mr.midaw@gmail.com>
2023-07-09 10:48:33 +0300

    heading & meta

--- frontend/app/components/screens/home/Home.tsx
+++ frontend/app/components/screens/home/Home.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from "react";
 
 import Layout from "@/components/layout/Layout";
+import Heading from "@/components/ui/heading/Heading";
+import { Meta } from "@/utils/meta";
 
 const Home: FC = () => {
 	return (
-		<Layout>
-			<div>Home page</div>
-		</Layout>
+		<Meta
+			title="Watch movies online"
+			description="Watch MovieApp movies and TV shows online or stream right to your browser."
+		>
+			<Heading title="Watch movies Online" className="gray-300 mb-8 text-xl" />
+		</Meta>
 	);
 };
 
